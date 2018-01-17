3b9253acb0cf55e699fe163a3d4de0a50c9e3e95
Satish <satish.r.venkat@gmail.com>
2018-01-17 20:11:37 +0530

    last modification

--- src/Sample.java
+++ src/Sample.java
@@ -2,17 +2,11 @@ public class Sample{
 	public static void main(String[] args){
 		
 		//It just prints the message 
-		//... more stateme
+		//... more statements
 		System.out.println("Hi EveryOne! A Spl welcome to you all");
 
 		System.out.println("Hi EveryOne");
 		
-<<<<<<< HEAD
-		//sample
-
-=======
-		//sample code 
->>>>>>> ece15878d780fb01206a424ee6dabb89d1cd0de2
 	}
 
 }
